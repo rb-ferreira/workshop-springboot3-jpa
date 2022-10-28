4aa17266495f8ab7734e9a113f7634b03ba9e179
Rafael Ferreira <rafael.bros.ferreira@gmail.com>
2022-10-28 14:40:14 -0300

    H2 database, test profile, JPA

--- src/main/java/br/com/estudando/course/CourseApplication.java
+++ src/main/java/br/com/estudando/course/CourseApplication.java
@@ -2,12 +2,13 @@ package br.com.estudando.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class CourseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
+		
+		System.out.println("App is Online!");
 	}
 
 }
