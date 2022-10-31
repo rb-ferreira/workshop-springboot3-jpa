e3b3f64aa58d2bc8fb9b6ef71ef71492e3a01b17
Rafael Ferreira <rafael.bros.ferreira@gmail.com>
2022-10-31 15:01:18 -0300

    Service layer, component registration

--- src/main/java/br/com/estudando/course/repositories/UserRepository.java
+++ src/main/java/br/com/estudando/course/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package br.com.estudando.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.estudando.course.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 	
