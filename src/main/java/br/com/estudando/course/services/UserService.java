9532eedd0f5d5963201aae3d1ac2a37bf30b55ab
Rafael Ferreira <rafael.bros.ferreira@gmail.com>
2022-11-07 11:16:06 -0300

    Exception handling - update

--- src/main/java/br/com/estudando/course/services/UserService.java
+++ src/main/java/br/com/estudando/course/services/UserService.java
@@ -12,6 +12,7 @@ import br.com.estudando.course.entities.User;
 import br.com.estudando.course.repositories.UserRepository;
 import br.com.estudando.course.services.exceptions.DatabaseException;
 import br.com.estudando.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -57,17 +58,29 @@ public class UserService {
 	}
 	
 	public User update (Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			
+			throw new ResourceNotFoundException(id);
+			
+			
+			
+		}
 		
 	}
 
 
 	private void updateData(User entity, User obj) {
+		
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
+		
+		
 				
 		
 	}
