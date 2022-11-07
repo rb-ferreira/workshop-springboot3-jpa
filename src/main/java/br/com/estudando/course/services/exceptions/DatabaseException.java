88fb07222e252ddbbada5580d07d7c0822bddfd6
Rafael Ferreira <rafael.bros.ferreira@gmail.com>
2022-11-07 11:07:43 -0300

    Exception handling - delete

---
+++ src/main/java/br/com/estudando/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,15 @@
+package br.com.estudando.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException(String msg) {
+		
+		super(msg);
+		
+	}
+	
+	
+
+}
